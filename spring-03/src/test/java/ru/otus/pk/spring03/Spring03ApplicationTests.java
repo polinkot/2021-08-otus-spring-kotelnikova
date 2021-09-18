b2bbb344caeffa8f0460bce6b566fb63df053999
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-09-18 18:20:07 +0300

    dz-spring-03

---
+++ spring-03/src/test/java/ru/otus/pk/spring03/Spring03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.pk.spring03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
