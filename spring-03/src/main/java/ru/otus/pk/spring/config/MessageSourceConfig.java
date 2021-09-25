b6fd5e7865d7127338fd25c148a8188d2b70f078
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-09-25 18:04:50 +0300

    spring boot

--- spring-03/src/main/java/ru/otus/pk/spring/config/MessageSourceConfig.java
+++ spring-03/src/main/java/ru/otus/pk/spring/config/MessageSourceConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
+import ru.otus.pk.spring.domain.UserLocale;
 
 import java.util.Locale;
 
@@ -13,18 +14,18 @@ public class MessageSourceConfig {
 
     @Bean("userLocale")
     @ConditionalOnProperty(value = "lang", havingValue = "ru")
-    public Locale getUserLocaleRu() {
-        return Locale.forLanguageTag("ru-RU");
+    public UserLocale getUserLocaleRu() {
+        return new UserLocale(Locale.forLanguageTag("ru-RU"));
     }
 
     @Bean("userLocale")
     @ConditionalOnProperty(value = "lang", havingValue = "en", matchIfMissing = true)
-    public Locale getUserLocaleEn() {
-        return Locale.US;
+    public UserLocale getUserLocaleEn() {
+        return new UserLocale(Locale.US);
     }
 
     @Bean("messageSourceAccessor")
-    public MessageSourceAccessor getMessageSourceAccessor(MessageSource messageSource, Locale userLocale) {
-        return new MessageSourceAccessor(messageSource, userLocale);
+    public MessageSourceAccessor getMessageSourceAccessor(MessageSource messageSource, UserLocale userLocale) {
+        return new MessageSourceAccessor(messageSource, userLocale.getLocale());
     }
 }
---
+++ spring-03/src/main/java/ru/otus/pk/spring/domain/UserLocale.java
@@ -0,0 +1,10 @@
+package ru.otus.pk.spring.domain;
+
+import lombok.Data;
+
+import java.util.Locale;
+
+@Data
+public class UserLocale {
+    private final Locale locale;
+}
