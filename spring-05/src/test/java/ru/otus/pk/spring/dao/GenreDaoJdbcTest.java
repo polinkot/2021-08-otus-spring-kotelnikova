ba7b4b526ada4ad681f35fdc113eb7c4c6c7ca13
k_pol <pk_work@mail.ru>
2021-10-15 18:45:54 +0300

    dz-05

--- spring-05/src/test/java/ru/otus/pk/spring/dao/GenreDaoJdbcTest.java
+++ spring-05/src/test/java/ru/otus/pk/spring/dao/GenreDaoJdbcTest.java
@@ -1,10 +1,12 @@
 package ru.otus.pk.spring.dao;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import ru.otus.pk.spring.domain.Genre;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,12 +18,22 @@ class GenreDaoJdbcTest {
     @Autowired
     private GenreDaoJdbc dao;
 
+    @DisplayName("возвращать ожидаемое количество жанров в БД")
     @Test
-    void count() {
+    void shouldReturnExpectedGenreCount() {
         int expectedCount = 1;
 
         int actualCount = dao.count();
         assertThat(actualCount).isEqualTo(expectedCount);
+    }
+
+    @DisplayName("добавлять жанр в БД")
+    @Test
+    void shouldInsertGenre() {
+        Genre expectedGenre = new Genre(2L, "Sci-fi");
+        dao.insert(expectedGenre);
+        Genre actualPerson = dao.getById(expectedGenre.getId());
+        Assertions.assertThat(actualPerson).usingRecursiveComparison().isEqualTo(expectedGenre);
     }
 
     @Test
