4a38bd3f9dc582462bbfeedf560fc266e3a202b7
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-12-30 12:47:24 +0300

    dz-11

---
+++ spring-11/src/test/java/ru/otus/pk/spring/controller/BookControllerTest.java
@@ -0,0 +1,72 @@
+package ru.otus.pk.spring.controller;
+
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import ru.otus.pk.spring.domain.Book;
+import ru.otus.pk.spring.repository.BookRepository;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@DisplayName("Контроллер для работы с книгами должен ")
+@WebFluxTest
+@ContextConfiguration(classes = {BookController.class})
+public class BookControllerTest {
+    private static final String ID = ObjectId.get().toString();
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private BookRepository repository;
+
+    @DisplayName("возвращать книгу по id")
+    @Test
+    public void findOneTest() {
+        webTestClient.get()
+                .uri("/books/" + ID)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+
+    @DisplayName("возвращать список книг")
+    @Test
+    public void findAllTest() {
+        webTestClient.get()
+                .uri("/books")
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+
+    @DisplayName("добавлять книгу")
+    @Test
+    public void saveTest() {
+        webTestClient.post()
+                .uri("/books")
+                .bodyValue(new Book())
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+
+    @DisplayName("удалять книгу")
+    @Test
+    public void deleteTest() {
+        given(repository.deleteById(anyString())).willReturn(Mono.empty());
+
+        webTestClient.delete()
+                .uri("/books/" + ID)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+}
