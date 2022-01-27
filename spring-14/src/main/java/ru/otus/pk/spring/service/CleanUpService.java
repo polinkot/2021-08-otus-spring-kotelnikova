0add0e036050f76837773de9063e6b4cd48dd734
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2022-01-27 20:11:23 +0300

    dz-14

--- spring-14/src/main/java/ru/otus/pk/spring/service/CleanUpService.java
+++ spring-14/src/main/java/ru/otus/pk/spring/service/CleanUpService.java
@@ -1,45 +1,23 @@
 package ru.otus.pk.spring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Pair;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.otus.pk.spring.domain.Author;
 import ru.otus.pk.spring.domain.Genre;
-import ru.otus.pk.spring.mongodomain.*;
-import ru.otus.pk.spring.repository.AuthorRepository;
+import ru.otus.pk.spring.mongodomain.Book2;
 
-import javax.persistence.EntityManager;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.function.Function;
-
-import static java.util.Map.ofEntries;
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.*;
+import java.util.List;
 
 @Service
 public class CleanUpService {
-
-//    @Autowired
-//    EntityManager entityManager;
-
-    @Autowired
-    AuthorRepository authorRepository;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
 
     public void cleanUp() throws Exception {
-//        List authors = entityManager.createQuery("Select t from " + Author.class + " t").getResultList();
-//        System.out.println("***********CleanUpService************authors");
-//        System.out.println(authors);
-
-//        jdbcTemplate.execute("update book set author_id = 2");
-
-
         List<Book2> books = jdbcTemplate.query("select * from book ", new BeanPropertyRowMapper<>(Book2.class));
         System.out.println("********** after cleanUp****************");
         System.out.println(books);
@@ -49,23 +27,6 @@ public class CleanUpService {
 
         List<Genre> genres = jdbcTemplate.query("select * from genre ", new BeanPropertyRowMapper<>(Genre.class));
         System.out.println(genres);
-
-//        List<Object[]> allAsMap = authorRepository.findAllAsMap();
-//        System.out.println(allAsMap);
-
-//        Map<String, Author> map = authorRepository.findByMongoIds(List.of(authors.get(0).getMongoId()));
-//        Map<String, Author> map = authorRepository.findByMongoIdIn(List.of(authors.get(0).getMongoId()))
-//                .collect(toMap(Author::getMongoId, identity()));
-//        Map<String, List<Author>> map = authorRepository.findByMongoIdIn(List.of(authors.get(0).getMongoId()))
-//                .collect(groupingBy(Author::getMongoId, toList()));
-//        System.out.println(map);
-
-//        Map<String, Author> map1 = ofEntries(authorRepository.findByMongoIds().toArray(Entry[]::new));
-//        System.out.println(map1);
-
-
-
-
 
         System.out.println("Выполняю завершающие мероприятия...");
         Thread.sleep(1000);
