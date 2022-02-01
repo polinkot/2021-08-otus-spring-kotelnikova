25f643623795e7207bbc184017b53e2def1693a6
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2022-02-01 14:10:35 +0300

    dz-15

--- spring-15/src/main/java/ru/otus/pk/spring/App.java
+++ spring-15/src/main/java/ru/otus/pk/spring/App.java
@@ -2,16 +2,11 @@ package ru.otus.pk.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import ru.otus.pk.spring.integration.IntegrationService;
 
 @SpringBootApplication
 public class App {
 
-    public static void main(String[] args) throws Exception {
-        ApplicationContext context = SpringApplication.run(App.class, args);
-
-        IntegrationService integrationService = context.getBean(IntegrationService.class);
-        integrationService.start();
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
--- spring-15/src/main/java/ru/otus/pk/spring/integration/IntegrationService.java
+++ spring-15/src/main/java/ru/otus/pk/spring/integration/IntegrationService.java
@@ -1,14 +1,14 @@
 package ru.otus.pk.spring.integration;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.otus.pk.spring.domain.Inspection;
 import ru.otus.pk.spring.domain.Report;
 
 import java.util.Collection;
-import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.joining;
@@ -23,24 +23,18 @@ public class IntegrationService {
 
     private final ReportGateway reportGateway;
 
-    public void start() throws InterruptedException {
-        ForkJoinPool pool = ForkJoinPool.commonPool();
+    @Async
+    @Scheduled(fixedRate = 1000)
+    public void start() {
+        Collection<Inspection> inspections = generateInspections();
+        System.out.println("---------New Inspections: " + inspections.stream()
+                .map(Inspection::toString)
+                .collect(joining("\n")));
 
-        while (true) {
-            Thread.sleep(7000);
-
-            pool.execute(() -> {
-                Collection<Inspection> inspections = generateInspections();
-                System.out.println("---------New Inspections: " + inspections.stream()
-                        .map(Inspection::toString)
-                        .collect(joining("\n")));
-
-                Collection<Report> reports = reportGateway.process(inspections);
-                System.out.println("---------Reports: " + reports.stream()
-                        .map(Report::toString)
-                        .collect(joining("\n")));
-            });
-        }
+        Collection<Report> reports = reportGateway.process(inspections);
+        System.out.println("---------Reports: " + reports.stream()
+                .map(Report::toString)
+                .collect(joining("\n")));
     }
 
     private static Collection<Inspection> generateInspections() {
