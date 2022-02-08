270c24ff8ef944cff50aaaaef538ce91d9cd8d3d
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2022-02-08 13:53:59 +0300

    dz-17

--- spring-17/src/main/resources/schema.sql
+++ spring-17/src/main/resources/schema.sql
@@ -1,22 +1,31 @@
-drop table if exists genre;
-create table genre(id bigserial, name varchar(255) not null unique);
-
-drop table if exists author;
-create table author(id bigserial, first_name varchar(255) not null, last_name varchar(255) not null);
-
+drop table if exists comment;
 drop table if exists book;
+drop table if exists author;
+drop table if exists genre;
+
+create table genre(id bigserial, name varchar(255) not null unique, PRIMARY KEY(id));
+
+create table author(id bigserial, first_name varchar(255) not null, last_name varchar(255) not null, PRIMARY KEY(id));
+
 create table book(id bigserial,
     name varchar(255) not null,
     author_id bigint not null,
     genre_id bigint not null,
-    foreign key (author_id) references author(id) on delete cascade,
-    foreign key (genre_id) references genre(id) on delete cascade
+    PRIMARY KEY(id),
+    CONSTRAINT fk_author
+      FOREIGN KEY(author_id)
+	  REFERENCES author(id) on delete cascade,
+	CONSTRAINT fk_genre
+      FOREIGN KEY(genre_id)
+	  REFERENCES genre(id) on delete cascade
 );
 
-drop table if exists comment;
 create table comment(id bigserial,
     text varchar(255) not null,
-    time datetime default current_timestamp,
+    time timestamp default current_timestamp,
     book_id bigint not null,
-    foreign key (book_id) references book(id) on delete cascade
-);
+    PRIMARY KEY(id),
+    CONSTRAINT fk_book
+      FOREIGN KEY(book_id)
+	  REFERENCES book(id) on delete cascade
+  );
