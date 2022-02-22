ca4dde77c478689d2c8838f37244e738f84df3c3
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2022-02-22 20:46:44 +0300

    cats-and-dogs

---
+++ cats-and-dogs/src/main/java/ru/otus/pk/spring/security/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package ru.otus.pk.spring.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().antMatchers(GET, "/api/v1/cats/**", "/api/v1/dogs/**").permitAll()
+                .and().authorizeRequests().antMatchers(DELETE, "/api/v1/**").hasAnyRole("ADMIN")
+                .and().authorizeRequests().antMatchers("/api/v1/**").authenticated()
+                .and().authorizeRequests().antMatchers("/**").denyAll()
+                .and().formLogin().defaultSuccessUrl("/api/v1/adoptions")
+                .and().logout().logoutSuccessUrl("/login").permitAll()
+                .and().exceptionHandling().accessDeniedPage("/error403");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
