8c5b4bae27581283c814456210e99ea722422a6e
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2022-02-24 21:30:20 +0300

    cats-and-dogs

--- cats-and-dogs/src/test/java/ru/otus/pk/spring/controller/DogControllerTest.java
+++ cats-and-dogs/src/test/java/ru/otus/pk/spring/controller/DogControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.pk.spring.domain.Dog;
 import ru.otus.pk.spring.service.DogService;
@@ -18,13 +20,14 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.otus.pk.spring.controller.Utils.asJsonString;
 import static ru.otus.pk.spring.domain.Gender.MALE;
 
-@DisplayName("Контроллер для работы с собаками должен ")
+@DisplayName("Контроллер для работы с собаками. ")
 @WebMvcTest(controllers = DogController.class)
 public class DogControllerTest {
 
@@ -34,9 +37,12 @@ public class DogControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
+    private UserDetailsService userDetailsService;
+
+    @MockBean
     private DogService service;
 
-    @DisplayName("возвращать ожидаемый список собак")
+    @DisplayName("для всех возвращать ожидаемый список собак")
     @Test
     public void shouldReturnExpectedDogsList() throws Exception {
         given(service.findAll()).willReturn(List.of(DOG));
@@ -48,7 +54,7 @@ public class DogControllerTest {
                 .andExpect(jsonPath("$[0].name", is(DOG.getName())));
     }
 
-    @DisplayName("возвращать собаку по id")
+    @DisplayName("для всех возвращать собаку по id")
     @Test
     public void shouldReturnExpectedDogById() throws Exception {
         given(service.findById(DOG.getId())).willReturn(DOG);
@@ -59,12 +65,13 @@ public class DogControllerTest {
                 .andExpect(jsonPath("$.name", is(DOG.getName())));
     }
 
-    @DisplayName("добавлять собаку")
+    @DisplayName("авторизованный может добавлять собаку")
+    @WithMockUser(username = "user", authorities = {"ROLE_USER"})
     @Test
-    public void shouldAddDog() throws Exception {
+    public void addForUser() throws Exception {
         given(service.save(any(Dog.class))).willReturn(DOG);
 
-        this.mockMvc.perform(post("/api/v1/dogs")
+        this.mockMvc.perform(post("/api/v1/dogs").with(csrf())
                 .contentType(APPLICATION_JSON)
                 .content(asJsonString(DOG)))
                 .andExpect(status().isCreated())
@@ -73,13 +80,25 @@ public class DogControllerTest {
                 .andExpect(jsonPath("$.name", is(DOG.getName())));
     }
 
-    @DisplayName("редактировать собаку")
+    @DisplayName("неавторизованный не может добавлять собаку")
     @Test
-    public void shouldUpdateDog() throws Exception {
+    public void notAddForNotAuth() throws Exception {
+        given(service.save(any(Dog.class))).willReturn(DOG);
+
+        this.mockMvc.perform(post("/api/v1/dogs")
+                .contentType(APPLICATION_JSON)
+                .content(asJsonString(DOG)))
+                .andExpect(status().isForbidden());
+    }
+
+    @DisplayName("авторизованный может редактировать собаку")
+    @WithMockUser(username = "user", authorities = {"ROLE_USER"})
+    @Test
+    public void updateForUser() throws Exception {
         given(service.findById(anyLong())).willReturn(DOG);
         given(service.save(any(Dog.class))).willReturn(DOG);
 
-        this.mockMvc.perform(put("/api/v1/dogs")
+        this.mockMvc.perform(put("/api/v1/dogs").with(csrf())
                 .contentType(APPLICATION_JSON)
                 .content(asJsonString(DOG)))
                 .andExpect(status().isOk())
@@ -88,12 +107,44 @@ public class DogControllerTest {
                 .andExpect(jsonPath("$.name", is(DOG.getName())));
     }
 
-    @DisplayName("удалять собаку")
+    @DisplayName("неавторизованный не может редактировать собаку")
     @Test
-    void shouldDeleteDog() throws Exception {
+    public void notUpdateForNotAuth() throws Exception {
+        given(service.findById(anyLong())).willReturn(DOG);
+        given(service.save(any(Dog.class))).willReturn(DOG);
+
+        this.mockMvc.perform(put("/api/v1/dogs")
+                .contentType(APPLICATION_JSON)
+                .content(asJsonString(DOG)))
+                .andExpect(status().isForbidden());
+    }
+
+    @DisplayName("админ может удалять собаку")
+    @WithMockUser(username = "admin", authorities = {"ROLE_ADMIN"})
+    @Test
+    void deleteForAdmin() throws Exception {
+        doNothing().when(service).deleteById(anyLong());
+
+        this.mockMvc.perform(delete("/api/v1/dogs/" + DOG.getId()).with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("не админ не может удалять собаку")
+    @WithMockUser(username = "user", authorities = {"ROLE_USER"})
+    @Test
+    void notDeleteForUser() throws Exception {
+        doNothing().when(service).deleteById(anyLong());
+
+        this.mockMvc.perform(delete("/api/v1/dogs/" + DOG.getId()).with(csrf()))
+                .andExpect(status().isForbidden());
+    }
+
+    @DisplayName("неавторизованный не может удалять собаку")
+    @Test
+    void notDeleteForNotAuth() throws Exception {
         doNothing().when(service).deleteById(anyLong());
 
         this.mockMvc.perform(delete("/api/v1/dogs/" + DOG.getId()))
-                .andExpect(status().isOk());
+                .andExpect(status().isForbidden());
     }
 }
